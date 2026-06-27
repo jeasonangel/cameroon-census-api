895c51f40ab3cd7ee61888b9ee3855f13e98ab17
jeasonangel <jeasonangel0@gmail.com>
2026-06-27 20:07:18 +0100

    change url

--- src/db/pool.ts
+++ src/db/pool.ts
@@ -2,8 +2,8 @@ import { Pool } from 'pg';
 import { config } from '../config';
 
 export const pool = new Pool(
-  config.databaseUrl
-    ? { connectionString: config.databaseUrl }
+  config.DATABASE_URL
+    ? { connectionString: config.DATABASE_URL }
     : {} // falls back to PGHOST/PGUSER/... env vars
 );
 
